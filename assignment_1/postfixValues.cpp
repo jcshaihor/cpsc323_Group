ad2cd307e73662ad55778a6fb768e9665f0916fa
Jesse Shaihor <jcshaihor@gmail.com>
2023-02-02 15:41:20 -0800

    commenting done

--- assignment_1/postfixValues.cpp
+++ assignment_1/postfixValues.cpp
@@ -1,28 +1,35 @@
+/*
+Group Names: Jay Vang, Jesse Shaihor, Cristian Salinas
+Homework 1
+Due Date: Feb 2,2023
+Purpose: this program reads an expression in postfix, evaluates the expression
+*/
 #include <iostream>
 #include <stack>
 #include <math.h>
-
+ 
 using namespace std;
-
+ 
 const int MAX = 100;
 int calc_Postfix(string expression);
-
-class Stack
+ 
+//stack implementation
+class Stack 
 {
     int topCounter;
-    public:
+public:
     int myStack[MAX];
-
-    Stack() { topCounter = -1;}
+ 
+    Stack() { topCounter = -1; }
     int top();
     bool push(int x);
     int pop();
     bool isEmpty();
 };
-
+ 
 bool Stack::push(int item)
 {
-    if (topCounter >= (MAX-1))
+    if (topCounter >= (MAX - 1))
     {
         cout << "Stack Overflow!";
         return false;
@@ -34,7 +41,7 @@ bool Stack::push(int item)
         return true;
     }
 }
-
+ 
 int Stack::pop()
 {
     if (topCounter < 0)
@@ -48,100 +55,96 @@ int Stack::pop()
         return item;
     }
 }
-
+ 
 int Stack::top()
 {
     if (topCounter == -1)
-        {
-            return 0;
-        }
+    {
+        return 0;
+    }
     else
     {
-    return myStack[topCounter];    
+        return myStack[topCounter];
     }
 }
-
+ 
 bool Stack::isEmpty()
 {
     return (topCounter < 0);
 }
 int main()
 {
-    int finalValue;
+    int finalValue; //Where postfix calculation will be stored
     char choice = 'y';
-    string s;
-
+    string s; //Where postfix expression will be stored
+ 
     while (choice == 'y')
     {
         string s;
-        cout << "Enter a post fix experssion with $ at the end: ";
+        cout << "Enter a post fix expression with $ at the end: ";
         cin >> s;
         finalValue = calc_Postfix(s);
         cout << "Value = " << finalValue << endl;
         cout << "Continue? (y/n)";
         cin >> choice;
     }
-
+ 
     return 0;
 }
-
-int calc_Postfix(string expression)
+ 
+int calc_Postfix(string expression) //Calculate Postfix expression 
 {
     class Stack stack;
-
-    int a = 5;
-    int b = 7;
-    int c = 2;
-    int d = 4;
+ 
+    //Integers used to calculate postfix expression
     int value1;
     int value2;
-
-    for (int i = 0; i < expression.length(); i++)
+ 
+    for (int i = 0; i < expression.length(); i++)   //Loop through string and push to stack based on char
     {
         switch (expression[i])
         {
-            case 'a':
-                stack.push(5);
-                break;
-            case 'b':
-                stack.push(7);
-                break;
-            case 'c':
-                stack.push(2);
-                break;
-            case 'd':
-                stack.push(4);
-                break;
-            case '+':
-                value1 = stack.top();
-                stack.pop();
-                value2 = stack.top();
-                stack.pop();
-                stack.push(value2 + value1);
-                break;
-            case '-':
-                value1 = stack.top();
-                stack.pop();
-                value2 = stack.top();
-                stack.pop();
-                stack.push(value2 - value1);
-                break;
-            case '*':
-                value1 = stack.top();
-                stack.pop();
-                value2 = stack.top();
-                stack.pop();
-                stack.push(value2 * value1);
-                break;
-            case '/':
-                value1 = stack.top();
-                stack.pop();
-                value2 = stack.top();
-                stack.pop();
-                stack.push(value2 / value1);
-                break;
+        case 'a':
+            stack.push(5);
+            break;
+        case 'b':
+            stack.push(7);
+            break;
+        case 'c':
+            stack.push(2);
+            break;
+        case 'd':
+            stack.push(4);
+            break;
+        case '+':
+            value1 = stack.top();
+            stack.pop();
+            value2 = stack.top();
+            stack.pop();
+            stack.push(value2 + value1);
+            break;
+        case '-':
+            value1 = stack.top();
+            stack.pop();
+            value2 = stack.top();
+            stack.pop();
+            stack.push(value2 - value1);
+            break;
+        case '*':
+            value1 = stack.top();
+            stack.pop();
+            value2 = stack.top();
+            stack.pop();
+            stack.push(value2 * value1);
+            break;
+        case '/':
+            value1 = stack.top();
+            stack.pop();
+            value2 = stack.top();
+            stack.pop();
+            stack.push(value2 / value1);
+            break;
         }
     }
     return stack.top();
 }
-
